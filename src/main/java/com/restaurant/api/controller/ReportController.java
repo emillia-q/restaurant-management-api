864275e0b13d34e329e9b8933658274d4f189961
Emilia Kura <emillia.kura@gmail.com>
2026-06-09 13:20:55 +0200

    feat: create ReportController

---
+++ src/main/java/com/restaurant/api/controller/ReportController.java
@@ -0,0 +1,12 @@
+package com.restaurant.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/v1/reports")
+public class ReportController {
+
+}
